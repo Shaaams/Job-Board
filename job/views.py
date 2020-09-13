6873128b285249d213837110a13e175d4fc0a345
Shaaams <shamsssherien@gmail.com>
2020-09-13 14:25:10 +0300

    add job views & urls

--- job/views.py
+++ job/views.py
@@ -1,3 +1,12 @@
+from django.http import request
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def jobs_list(request):
+    pass
+
+
+def job_details(request, id):
+    pass
